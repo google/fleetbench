7b02c862f632e24382a7f17355f26319cef90d72
Yuying Li <liyuying@google.com>
2023-04-21 11:25:47 -0700

    2 of N changes to add Compression benchmark -- defines corpora folder hierarchy and supports multiple inputs
    PiperOrigin-RevId: 526090531
    Change-Id: If452e7ed73f23ddbca73f3d7e048db0c51d0a87e

--- fleetbench/compression/compression_benchmark.cc
+++ fleetbench/compression/compression_benchmark.cc
@@ -12,14 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstddef>
+#include <filesystem>  // NOLINT
 #include <fstream>
 #include <ios>
 #include <iterator>
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "absl/log/check.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
 #include "benchmark/benchmark.h"
 #include "fleetbench/compression/algorithms.h"
 
@@ -37,51 +42,87 @@ bool GetContents(const std::string& file_path, std::string* output) {
   *output = std::move(content);
   return true;
 }
+
+// Reads all files under a given "file_path" directory and returns as a vector
+std::vector<std::string> GetCorpora(const absl::string_view file_path) {
+  const std::filesystem::path sandbox{file_path};
+  std::vector<std::string> corpora;
+  for (auto const& dir_entry : std::filesystem::directory_iterator{sandbox}) {
+    std::string corpus;
+    CHECK(GetContents(dir_entry.path(), &corpus));
+    corpora.push_back(corpus);
+  }
+  return corpora;
+}
+
 }  // namespace
 
-static void BM_Compress_Snappy(benchmark::State& state) {
-  std::string file_name =
-      "fleetbench/compression/corpora/alice29.txt";
+static constexpr absl::string_view kCorporaPath =
+    "fleetbench/compression/corpora/";
 
-  std::string data;
-  CHECK(GetContents(file_name, &data));
+template <class... Args>
+static void BM_Compress_Snappy(benchmark::State& state, Args&&... args) {
+  auto args_tuple = std::make_tuple(std::move(args)...);
 
-  auto snappy_compressor = std::make_unique<SnappyCompressor>();
+  std::string binary_directory = std::get<0>(args_tuple);
+  std::string file_path = absl::StrCat(kCorporaPath, binary_directory);
+
+  // Read in benchmark corpus
+  auto corpora = GetCorpora(file_path);
 
   // Reserve space to store string after compression and decompression.
-  std::string compressed, decompressed;
+  std::vector<std::string> compressed(corpora.size()),
+      decompressed(corpora.size());
+
+  auto snappy_compressor = std::make_unique<SnappyCompressor>();
 
   for (auto _ : state) {
     //  Compress file
-    snappy_compressor->Compress(data, &compressed);
+    for (size_t i = 0; i < corpora.size(); i++) {
+      benchmark::DoNotOptimize(
+          snappy_compressor->Compress(corpora[i], &compressed[i]));
+    }
   }
-  // Decompress file
-  snappy_compressor->Decompress(compressed, &decompressed);
-  CHECK_EQ(data, decompressed);
+  // Decompress file and check corretness
+  for (size_t i = 0; i < compressed.size(); i++) {
+    snappy_compressor->Decompress(compressed[i], &decompressed[i]);
+    CHECK_EQ(corpora[i], decompressed[i]);
+  }
 }
 
-static void BM_Decompress_Snappy(benchmark::State& state) {
-  std::string file_name =
-      "fleetbench/compression/corpora/alice29.txt";
+template <class... Args>
+static void BM_Decompress_Snappy(benchmark::State& state, Args&&... args) {
+  auto args_tuple = std::make_tuple(std::move(args)...);
 
-  std::string data;
-  CHECK(GetContents(file_name, &data));
+  std::string binary_directory = std::get<0>(args_tuple);
+  std::string file_path = absl::StrCat(kCorporaPath, binary_directory);
+
+  // Read in benchmark corpus
+  auto corpora = GetCorpora(file_path);
+
+  // Reserve space to store string after compression and decompression.
+  std::vector<std::string> compressed(corpora.size()),
+      decompressed(corpora.size());
 
   auto snappy_compressor = std::make_unique<SnappyCompressor>();
 
-  // Reserve space to store string after compression and decompression.
-  std::string compressed, decompressed;
-
-  snappy_compressor->Compress(data, &compressed);
+  for (size_t i = 0; i < corpora.size(); i++) {
+    snappy_compressor->Compress(corpora[i], &compressed[i]);
+  }
 
   for (auto _ : state) {
-    snappy_compressor->Decompress(compressed, &decompressed);
+    for (size_t i = 0; i < compressed.size(); i++) {
+      benchmark::DoNotOptimize(
+          snappy_compressor->Decompress(compressed[i], &decompressed[i]));
+    }
   }
-  CHECK_EQ(data, decompressed);
+  for (size_t i = 0; i < decompressed.size(); i++) {
+    CHECK_EQ(corpora[i], decompressed[i]);
+  }
 }
 
-BENCHMARK(BM_Compress_Snappy);
-BENCHMARK(BM_Decompress_Snappy);
+BENCHMARK_CAPTURE(BM_Compress_Snappy, A, "Snappy-COMPRESS-A");
+BENCHMARK_CAPTURE(BM_Decompress_Snappy, A, "Snappy-DECOMPRESS-A");
 
 }  // namespace compression
 }  // namespace fleetbench
